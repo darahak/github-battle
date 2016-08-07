9ec567589631aec9a772af6fe7b2e7770124a78a
Dara Hak <dara.hak@gmail.com>
2016-08-07 17:57:36 +0200

    Milestone: "this.props.children and getting started with React Router".

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>Hello from Home!</div>
+        );
+    }
+}
+
+module.exports = Home;
--- app/index.js
+++ app/index.js
@@ -1,51 +1,9 @@
-const USER_DATA = {
-    name: 'Tyler McGinnis',
-    username: 'tylermcginnis',
-    image: 'https://avatars0.githubusercontent.com/u/2933439?v=3&s=460'
-};
-
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-class ProfilePic extends React.Component {
-    render() {
-        return <img src={this.props.imageUrl} style={{width: 100, height: 100}} />
-    }
-}
-
-class ProfileLink extends React.Component {
-    render() {
-        return (
-            <div>
-                <a href={'https://www.github.com/' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        );
-    }
-}
-
-class ProfileName extends React.Component {
-    render() {
-        return (
-            <div>{this.props.name}</div>
-        );
-    }
-}
-
-class Avatar extends React.Component {
-    render() {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        );
-    }
-}
+const routes = require('./config/routes');
 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
